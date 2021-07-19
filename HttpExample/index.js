6b13b40d6661f55cd9605a50bbf2d4168a1facad
kondoumh <kondoumh@gmail.com>
2021-07-19 13:30:05 +0900

    Add cosmos db bindings

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -6,6 +6,14 @@ module.exports = async function (context, req) {
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    if (name) {
+        context.bindings.outputDocument = JSON.stringify({
+            // create a random ID
+           id: new Date().toISOString() + Math.random().toString().substr(2, 8),
+           name: name
+        });
+    }
+
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
