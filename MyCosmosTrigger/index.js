f923dd199988b381bdd51823050714646064d2c7
kondoumh <kondoumh@gmail.com>
2021-07-20 15:52:10 +0900

    output modified count

--- MyCosmosTrigger/index.js
+++ MyCosmosTrigger/index.js
@@ -1,5 +1,6 @@
 module.exports = async function (context, documents) {
     if (!!documents && documents.length > 0) {
+        context.log('Documents modified: ', documents.length);
         context.log('Document Id: ', documents[0].id);
     }
 }
