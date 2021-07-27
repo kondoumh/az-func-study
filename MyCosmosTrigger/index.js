9b55d983a77ffe6a05005619d434aae56221aef1
kondoumh <kondoumh@gmail.com>
2021-07-27 11:42:17 +0900

    Add event hubs binding

--- MyCosmosTrigger/index.js
+++ MyCosmosTrigger/index.js
@@ -2,5 +2,8 @@ module.exports = async function (context, documents) {
     if (!!documents && documents.length > 0) {
         context.log('Documents modified: ', documents.length);
         context.log('Document Id: ', documents[0].id);
+
+        context.bindings.outputEventHubMessage = "Message created for ID: " + documents[0].id;
+        context.done();
     }
 }
